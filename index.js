69facac7cf96a8c36290f6082a62d4558fb4c7f0
shannone <shannone426@gmail.com>
2022-08-05 13:12:50 -0400

    lab complete

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
 //  3)  else if num over 2000 feet && num is less then 2500
 //      returns  'I will gladly take your thirty bucks.'
 //  4)  else if num over 2500 feet return 'No can do.'
-function scuberGreetingForFeet(n){
+const scuberGreetingForFeet = (n) =>{
   if (n <= 400) {
     return "This one is on me!"
   }else if (n >= 400 && n <= 2000) {
@@ -24,10 +24,22 @@ function scuberGreetingForFeet(n){
   };
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// 1) when the city (str) is 'NYC', return "Ok, sounds good." : "No go."
+//    I need way more practice with this syntax lol 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+const ternaryCheckCity = (str) => {
+  return (str === "NYC" ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+// 1) should return "Thank you so much." if the tip is generous
+// 2) should return "Thank you." if the tip is not as generous:
+// 3) should return "Bye." if anything else => default
+const switchOnCharmFromTip = (tip) => {
+  switch (tip) {
+    case "generous" :
+      return "Thank you so much.";
+      case "not as generous" :
+        return "Thank you.";
+        default : return "Bye.";
+  };
+};
