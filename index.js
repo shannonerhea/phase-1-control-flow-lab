33bf92c2f69ac838191410ff4279822dfc98db3f
shannone <shannone426@gmail.com>
2022-08-05 12:46:24 -0400

    scuberfeetdone

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+
+
+
+
+
+
+//  1) if num is less than or equal to 400 feet returns 'This one is on me!'
+//  2) else if num is between 400 and 2000 feet: (condition && condition)
+//     returns 'That will be twenty bucks.'
+//  3)  else if num over 2000 feet && num is less then 2500
+//      returns  'I will gladly take your thirty bucks.'
+//  4)  else if num over 2500 feet return 'No can do.'
+function scuberGreetingForFeet(n){
+  if (n <= 400) {
+    return "This one is on me!"
+  }else if (n >= 400 && n <= 2000) {
+    return "That will be twenty bucks."
+  } else if (n > 2000 && n <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (n > 2500) {
+    return "No can do.";
+  };
 }
 
 function ternaryCheckCity(){
